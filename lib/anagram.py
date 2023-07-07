e1bbb9dd003e157cb9882c965282369234797997
Shanley Caswell <shanleycaswell@gmail.com>
2023-07-06 20:06:37 -0400

    tests passing

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        return [w for w in words if self._is_anagram(w)]
+        
+    def _is_anagram(self, word):
+        return sorted(self.word) == sorted(word.lower()) and self.word != word.lower()
+
+hello = Anagram('hello')
